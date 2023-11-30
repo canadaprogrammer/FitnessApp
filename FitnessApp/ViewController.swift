2b7251fbcc73d860b691e10015cdbf31ac337298
Jin Park <jinpark1504@gmail.com>
2023-11-29 22:37:28 -0800

    Add Components to Setting View

--- FitnessApp/ViewController.swift
+++ FitnessApp/ViewController.swift
@@ -8,13 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var roundsCountLabel: UILabel!
+    @IBOutlet var warmUpPicker: UIDatePicker!
+    @IBOutlet var workPicker: UIDatePicker!
+    @IBOutlet var restPicker: UIDatePicker!
+    @IBOutlet var coolDownPicker: UIDatePicker!
+    
+    var roundsCount: Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Test")
+        roundsCountLabel.text = String(roundsCount)
     }
-
+    
+    @IBAction func roundsSliderChanged(_ sender: UISlider) {
+        roundsCount = Int(sender.value)
+        roundsCountLabel.text = String(roundsCount)
+    }
+    
 
 }
 
