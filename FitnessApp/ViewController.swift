b63b2d82f418a1b9240ec21aa106138af8dac787
Jin Park <jinpark1504@gmail.com>
2023-11-30 21:37:20 -0800

    Add UIPickerView for minutes and seconds

--- FitnessApp/ViewController.swift
+++ FitnessApp/ViewController.swift
@@ -7,19 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     @IBOutlet var roundsCountLabel: UILabel!
-    @IBOutlet var warmUpPicker: UIDatePicker!
-    @IBOutlet var workPicker: UIDatePicker!
-    @IBOutlet var restPicker: UIDatePicker!
-    @IBOutlet var coolDownPicker: UIDatePicker!
+    @IBOutlet var warmUpPicker: UIPickerView!
     
-    var roundsCount: Int = 1
+    var pickerData:[[Int]] = [[Int]]()
+    let range:[Int] = Array(0..<60)
+    
+    var roundsCount: Int = 0
+    
+    var labelTexts = ["Min", "Sec"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         roundsCountLabel.text = String(roundsCount)
+        
+        self.warmUpPicker.delegate = self
+        self.warmUpPicker.dataSource = self
+        
+        pickerData = [range, range]
+        
+        warmUpPicker.setPickerLabels(labelSize: 20.0, labelTexts: labelTexts)
+        
     }
     
     @IBAction func roundsSliderChanged(_ sender: UISlider) {
@@ -27,6 +37,38 @@ class ViewController: UIViewController {
         roundsCountLabel.text = String(roundsCount)
     }
     
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData[component].count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(pickerData[component][row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(pickerData[component][row])
+    }
 }
 
+extension UIPickerView {
+    func setPickerLabels(labelSize: Double, labelTexts: [String]) {
+        let font = UIFont.systemFont(ofSize: labelSize)
+        let fontSize: CGFloat = font.pointSize
+        let labelWidth: CGFloat = self.frame.width / CGFloat(self.numberOfComponents)
+        let y = (self.frame.size.height / 2) - (fontSize / 2)
+        
+        for index in 0..<labelTexts.count {
+            let label = UILabel(frame: CGRect(x: (self.frame.origin.x + labelWidth) * CGFloat(index) * 0.43, y: CGFloat(y), width: labelWidth, height: CGFloat(fontSize)))
+            label.text = labelTexts[index]
+            label.textAlignment = .right
+            self.addSubview(label)
+        }
+    }
+}
